e1a4071dd85e26945c265eca34c92648817555e4
dasha <dasha15042009@yandex.ru>
2022-08-18 15:04:14 +0300

    HelloTvar

---
+++ hello console/HelloDasha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Привет, лучший человек на планете");
+}
+else 
+{
+    Console.Write("Привет, тварь, ");
+    Console.Write(username);
+}
